242185637fbce95aee002d137cfbd0d08b52b9fe
hha2907 <ha.dh290702@gmail.com>
2023-08-09 22:48:25 +0300

    update loading skeleton

--- src/components/movie/MovieCard.js
+++ src/components/movie/MovieCard.js
@@ -4,6 +4,7 @@ import Button from '../button/Button';
 import { tmdbAPI } from '../../apiConfig/config';
 import { withErrorBoundary } from 'react-error-boundary';
 import PropTypes from 'prop-types';
+import LoadingSkeleton from 'loading/LoadingSkeleton';
 
 const MovieCard = ({ item }) => {
     const { title, poster_path, release_date, vote_average, id } = item;
@@ -40,3 +41,25 @@ function FallbackComponent() {
 export default withErrorBoundary(MovieCard, {
     FallbackComponent,
 });
+
+export const MovieCardSkeleton = () => {
+    return (
+        <div className="movie-card flex flex-col rounded-lg p-3 bg-slate-800 text-white h-full select-none">
+            <LoadingSkeleton width="100%" height="250px" radius="8px" className="mb-5"></LoadingSkeleton>
+            <div className="flex flex-col flex-1">
+                <h3 className="text-xl font-bold mb-3">
+                    <LoadingSkeleton width="100%" height="20px"></LoadingSkeleton>
+                </h3>
+                <div className="flex items-center justify-between text-sm opacity-50 mb-10">
+                    <span>
+                        <LoadingSkeleton width="50px" height="10px"></LoadingSkeleton>
+                    </span>
+                    <span>
+                        <LoadingSkeleton width="30px" height="10px"></LoadingSkeleton>
+                    </span>
+                </div>
+                <LoadingSkeleton width="100%" height="45px" radius="6px"></LoadingSkeleton>
+            </div>
+        </div>
+    );
+};
--- src/components/movie/MovieList.js
+++ src/components/movie/MovieList.js
@@ -1,32 +1,51 @@
 import React from 'react';
 import { SwiperSlide, Swiper } from 'swiper/react';
-import 'swiper/scss';
-import MovieCard from './MovieCard';
+import MovieCard, { MovieCardSkeleton } from './MovieCard';
 import useSWR from 'swr';
-import { fetcher, tmdbAPI } from '../../apiConfig/config';
+import { fetcher, tmdbAPI } from 'apiConfig/config';
 import PropTypes from 'prop-types';
 import { withErrorBoundary } from 'react-error-boundary';
-//  https://api.themoviedb.org/3/movie/now_playing
-// API KEY=5504b60886ea3b781bb83f3415f99a3d
 
 const MovieList = ({ type = 'now_playing' }) => {
-    const { data } = useSWR(tmdbAPI.getMovieList(type), fetcher);
-
+    const { data, error } = useSWR(tmdbAPI.getMovieList(type), fetcher);
+    const isLoading = !data && !error;
     const movies = data?.results || [];
     return (
         <div className="movie-list">
-            <Swiper grabCursor={'true'} spaceBetween={40} slidesPerView={'auto'}>
-                {movies.length > 0 &&
-                    movies.map((item) => (
-                        <SwiperSlide key={item.id}>
-                            <MovieCard item={item}></MovieCard>
+            {isLoading && (
+                <>
+                    <Swiper grabCursor={'true'} spaceBetween={40} slidesPerView={'auto'}>
+                        <SwiperSlide>
+                            <MovieCardSkeleton></MovieCardSkeleton>
                         </SwiperSlide>
-                    ))}
-            </Swiper>
+                        <SwiperSlide>
+                            <MovieCardSkeleton></MovieCardSkeleton>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <MovieCardSkeleton></MovieCardSkeleton>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <MovieCardSkeleton></MovieCardSkeleton>
+                        </SwiperSlide>
+                        <SwiperSlide>
+                            <MovieCardSkeleton></MovieCardSkeleton>
+                        </SwiperSlide>
+                    </Swiper>
+                </>
+            )}
+            {!isLoading && (
+                <Swiper grabCursor={'true'} spaceBetween={40} slidesPerView={'auto'}>
+                    {movies.length > 0 &&
+                        movies.map((item) => (
+                            <SwiperSlide key={item.id}>
+                                <MovieCard item={item}></MovieCard>
+                            </SwiperSlide>
+                        ))}
+                </Swiper>
+            )}
         </div>
     );
 };
-
 MovieList.propTypes = {
     type: PropTypes.string.isRequired,
 };
